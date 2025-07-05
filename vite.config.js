5d77089e068b5a910019bc7714ae6136effe4b27
Harshitshyamsukha <141991090+Harshitshyamsukha@users.noreply.github.com>
2025-07-05 15:01:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
   root: './src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
   },
+  plugins: [react()],
 });
