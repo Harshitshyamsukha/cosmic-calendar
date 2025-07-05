9198f1ce3c10828f9f911f7c8a80aecdb5eab28d
Harshit Shyamsukha <harshitshyamsukha@gmail.com>
2025-07-05 14:50:14 +0530

    Set Vite root to src for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true
-  }
+  root: './src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 });
